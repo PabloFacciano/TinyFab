58450f83ea5c0119ac7a7ed14fa14e927436006f
Pablo Facciano <pablo.facciano@globant.com>
2024-06-30 18:37:28 -0300

    Vitest included

--- TinyFab/vite.config.js
+++ TinyFab/vite.config.js
@@ -4,4 +4,11 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  environment: 'happy-dom',
+  test:{
+    globals:true,
+    coverage: {
+      reporter: ['text', 'json', 'html'],
+    }
+  }
+ })
