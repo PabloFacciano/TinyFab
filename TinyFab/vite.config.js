14d1b4bffc9125ea8533b58b5394a9ec613a901c
Pablo Facciano <pablo.facciano@globant.com>
2024-07-07 17:23:59 -0300

    vue tester

--- TinyFab/vite.config.js
+++ TinyFab/vite.config.js
@@ -1,16 +1,6 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  environment: 'happy-dom',
-  test:{
-    globals:true,
-    coverage: {
-      reporter: ['text', 'json', 'html'],
-    }
-  }
- })
-
- 
+});
--- TinyFab/vitest.config.js
+++ TinyFab/vitest.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vitest/config';
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+  plugins: [vue()],
   test: {
+    environment: 'happy-dom',  // or 'jsdom'
     coverage: {
       provider: 'v8', // or 'istanbul' if you prefer
       reporter: ['text', 'json', 'html'], // choose the reporters you need
