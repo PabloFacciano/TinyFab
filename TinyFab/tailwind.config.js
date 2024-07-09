5c4f84973934d61bd142a01434f770c9690c9a72
Pablo Facciano <pablo.facciano@globant.com>
2024-07-09 15:43:04 -0300

    tailwind fix

--- TinyFab/tailwind.config.js
+++ TinyFab/tailwind.config.js
@@ -1,6 +1,5 @@
 module.exports = {
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  media: false, // or 'media' or 'class'
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
